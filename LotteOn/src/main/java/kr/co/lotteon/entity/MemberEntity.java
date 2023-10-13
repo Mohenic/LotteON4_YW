77faca2161767d5dcfd435a62869898898673271
wjdals1217 <wjdals1217@gmail.com>
2023-10-13 21:40:39 +0900

    memberentity 수정중

--- LotteOn/src/main/java/kr/co/lotteon/entity/MemberEntity.java
+++ LotteOn/src/main/java/kr/co/lotteon/entity/MemberEntity.java
@@ -1,4 +1,47 @@
 package kr.co.lotteon.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "lo_member")
 public class MemberEntity {
+
+    @Id
+    private String uid;
+    private String pass;
+    private String name;
+    private String gender;
+    private String hp;
+    private String email;
+    private String type;
+    private String point;
+    private String level;
+    private String zip;
+    private String addr1;
+    private String addr2;
+    private String company;
+    private String ceo;
+    private String bizRegNum;
+    private String comRegNum;
+    private String tel;
+    private String manager;
+    private String managerHp;
+    private String fax;
+    private String regip;
+    private String wdate;
+    private String rdate;
+    private String etc1;
+    private String etc2;
+    private String etc3;
+    private String etc4;
+    private String etc5;
+
 }
--- LotteOn/src/main/java/kr/co/lotteon/repository/MemberRepository.java
+++ LotteOn/src/main/java/kr/co/lotteon/repository/MemberRepository.java
@@ -3,6 +3,7 @@ package kr.co.lotteon.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface MemberRepository {
 
