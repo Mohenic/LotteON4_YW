9bf3cb37a82f8687526d5eebc1aca2b5d5600442
wjdals1217 <wjdals1217@gmail.com>
2023-10-13 16:43:34 +0900

    테스트

---
+++ LotteOn/src/main/java/kr/co/lotteon/config/RootConfig.java
@@ -0,0 +1,10 @@
+package kr.co.lotteon.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RootConfig {
+
+
+}
