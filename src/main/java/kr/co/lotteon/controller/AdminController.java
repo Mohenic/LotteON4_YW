f22a60ff4ea559341c38f0037fa345e004fcd0b5
Mohenic <syw077@gmail.com>
2023-10-16 17:45:40 +0900

    1차 카테고리 출력

--- src/main/java/kr/co/lotteon/controller/AdminController.java
+++ src/main/java/kr/co/lotteon/controller/AdminController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import kr.co.lotteon.dto.product.ProductDTO;
 import kr.co.lotteon.entity.product.ProductCate1Entity;
 import kr.co.lotteon.service.AdminService;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
+@Log4j2
 @Controller
 public class AdminController {
 
@@ -23,7 +25,13 @@ public class AdminController {
         return "/admin/index";
     }
     @GetMapping("/admin/product/register")
-    public String register(){
+    public String register(Model model){
+
+        List<ProductCate1Entity> setCate1 = adminService.cateList();
+
+        log.info("setCate1 : " + setCate1);
+
+        model.addAttribute("setCate1",setCate1);
 
         return "/admin/product/register";
     }
