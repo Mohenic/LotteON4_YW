44a2df002aa7cb668bcb64d125aa508268ad0671
unknown <https://github.com/pdillona/Spring_SpringBoot.git>
2023-10-17 10:45:28 +0900

    product 페이징중 2

--- src/main/java/kr/co/lotteon/repository/product/ProductRepository.java
+++ src/main/java/kr/co/lotteon/repository/product/ProductRepository.java
@@ -1,7 +1,5 @@
 package kr.co.lotteon.repository.product;
 
-import kr.co.lotteon.dto.product.ProductCate1DTO;
-import kr.co.lotteon.entity.product.ProductCate1Entity;
 import kr.co.lotteon.entity.product.ProductEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -12,9 +10,9 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ProductRepository extends JpaRepository<ProductEntity, Integer> {
 
-    public Page<ProductEntity> findProductEntityByProdNoAndProdCate1(int prodNo, String cate1, Pageable pageable);
+    public Page<ProductEntity> findAllByprodCate1(String cate1, Pageable pageable);
     
     
-    public Page<ProductEntity> findProductEntityByProdNoAndProdCate1AndProdCate2(int prodNo, String cate1, String cate2, Pageable pageable);
+    public Page<ProductEntity> findByProdNoAndProdCate1AndProdCate2(int prodNo, String cate1, String cate2, Pageable pageable);
     
 }
