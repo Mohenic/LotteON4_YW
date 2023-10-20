bc75ff058ad8bf83f954372c60f14e74b42e0d9b
Mohenic <syw077@gmail.com>
2023-10-20 12:40:15 +0900

    admin cs notice list paging,delete 구현, write 구현중

--- src/main/java/kr/co/lotteon/mapper/AdminCSMapper.java
+++ src/main/java/kr/co/lotteon/mapper/AdminCSMapper.java
@@ -7,11 +7,24 @@ import java.util.List;
 
 @Mapper
 public interface AdminCSMapper {
+
+    // notice list
     public List<CsArticleNoticeDTO> selectNoticeList(int start);
     public List<CsArticleNoticeDTO> NoticeListcs(int start);
     public List<CsArticleNoticeDTO> NoticeListsafety(int start);
     public List<CsArticleNoticeDTO> NoticeListdanger(int start);
     public List<CsArticleNoticeDTO> NoticeListeveresult(int start);
     public int selectCountTotal();
+    public int selectCountTotalCate(String cate2);
+
+    // notice view
+
+    public CsArticleNoticeDTO NoticeView(int no);
+
+    // notice write
+
+    public CsArticleNoticeDTO NoticeWrite(CsArticleNoticeDTO dto, String regip);
+
+
 
 }
