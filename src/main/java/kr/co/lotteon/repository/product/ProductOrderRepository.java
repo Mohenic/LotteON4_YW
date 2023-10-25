017f166624b8941fc377aeaf294c43854875c65e
wjdals1217 <wjdals1217@gmail.com>
2023-10-25 12:42:27 +0900

    주문내역 기간별 조회

--- src/main/java/kr/co/lotteon/repository/product/ProductOrderRepository.java
+++ src/main/java/kr/co/lotteon/repository/product/ProductOrderRepository.java
@@ -4,10 +4,22 @@ import kr.co.lotteon.entity.product.ProductOrderEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
 
 @Repository
 public interface ProductOrderRepository extends JpaRepository<ProductOrderEntity, Integer> {
 
     int countByOrdUid(String ordUid);
+
+    @Query("SELECT COUNT(*) FROM ProductOrderEntity o " +
+            "WHERE o.ordUid =:ordUid AND " +
+            "o.ordDate >= :beginDate AND o.ordDate <= :endDate")
+    int countByOrdUidAndDate(
+            @Param("ordUid") String ordUid,
+            @Param("beginDate") LocalDateTime beginDate,
+            @Param("endDate") LocalDateTime endDate);
 }
