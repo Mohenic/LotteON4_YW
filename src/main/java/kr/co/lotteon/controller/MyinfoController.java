8441d49c450decc36780a9cba74608bdd48d8070
wjdals1217 <wjdals1217@gmail.com>
2023-10-27 20:14:37 +0900

    my page 기능구현

--- src/main/java/kr/co/lotteon/controller/MyinfoController.java
+++ src/main/java/kr/co/lotteon/controller/MyinfoController.java
@@ -2,12 +2,15 @@ package kr.co.lotteon.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
 import kr.co.lotteon.dto.MemberDTO;
+import kr.co.lotteon.dto.my.MyInfoDTO;
+import kr.co.lotteon.service.MyService;
 import kr.co.lotteon.service.MyinfoService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -17,6 +20,9 @@ public class MyinfoController {
 
     @Autowired
     private MyinfoService iService;
+
+    @Autowired
+    private MyService myService; // �
 
     @GetMapping("/my/info")
     public String info(HttpServletRequest req, Model model){
@@ -28,8 +34,28 @@ public class MyinfoController {
         return "/my/info";
     }
 
+
+    @ModelAttribute("myInfo")
+    public MyInfoDTO myinfo(String uid,
+                            String ordUid,
+                            String writer){
+
+        log.info("uid : "+uid);
+        log.info("ordUid : "+ordUid);
+        log.info("writer : "+writer);
+        MyInfoDTO member = null;
+        if(uid != null){
+            member = myService.findMyInfo(uid);
+        }else if(ordUid != null){
+            member = myService.findMyInfo(ordUid);
+        }else if(writer != null){
+            member = myService.findMyInfo(writer);
+        }
+        log.info("member : "+member);
+        return member;
+    }
     @PostMapping("/my/info/modify")
-    public String myinfoModify(MemberDTO dto){
+    public String myinfoModify(MemberDTO dto, Model model){
         dto.setEmail(dto.getEmail1() + "@" + dto.getEmail2());
         dto.setHp(dto.getHp1() + "-" + dto.getHp2() + "-" + dto.getHp3());
 
--- src/main/java/kr/co/lotteon/dto/my/MyInfoDTO.java
+++ src/main/java/kr/co/lotteon/dto/my/MyInfoDTO.java
@@ -11,6 +11,9 @@ import lombok.*;
 @Builder
 public class MyInfoDTO {
 
+
+
+
     private int point;
     private int count_b; //쿠폰 개수
     private int count_c; // 주문 개수
